cfa917fb54ef86826fd542c3151b8ac805bdc4dd
Glenpl <kubamolinski@gmail.com>
2016-07-24 21:56:53 +0200

    Wyzwanie.__init__ with validation

--- wyzwanie/wyzwanie.py
+++ wyzwanie/wyzwanie.py
@@ -3,8 +3,20 @@ __all__ = ['Wyzwanie']
 
 
 class Wyzwanie:  # TODO singleton?
-    def __init__(self):
-        pass
+    def __init__(self, exercise_id=None, difficulty=None, **kwargs):
+        self._validate_init_args(exercise_id, difficulty)
+        self._exercise_id = exercise_id
+        self._exercise_difficulty = difficulty
+
+    def _validate_init_args(self, exercise_id, difficulty):
+        if not exercise_id or not difficulty:
+            raise ValueError('Both exercise id and difficulty have to be specified')
+        try:
+            difficulty = difficulty.lower()
+        except AttributeError:
+            raise TypeError('Difficulty has to be one of ("easy", "hard")')
+        if difficulty not in ('easy', 'hard'):
+            raise TypeError('Difficulty has to be one of ("easy", "hard")')
 
     def main_function(self):
         pass
