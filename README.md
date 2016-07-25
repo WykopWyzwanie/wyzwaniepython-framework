9c39244e86d18c44ccdb7b5f9a9650b1a3d34f7f
Glenpl <kubamolinski@gmail.com>
2016-07-25 20:54:23 +0200

    updated readme, added python_version argument to Wyzwanie.__init__

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # wyzwaniepython-framework
+
+Przykład użycia:
+```python
+from wyzwanie_wykop import Wyzwanie
+
+app = Wyzwanie(2, 'easy', 3)  # numer zadania, poziom trudności, wersja pythona
+
+
+def add(a, b):
+  return a + b
+
+
+@app.main_function
+def sample_function(console_arg1, console_arg2):
+  return add(console_arg1, console_arg2)
+
+
+if __name__ == '__main__':
+  app.run()
+
+```
--- wyzwanie/wyzwanie.py
+++ wyzwanie/wyzwanie.py
@@ -4,12 +4,12 @@ __all__ = ['Wyzwanie']
 
 
 class Wyzwanie:  # TODO singleton?
-    def __init__(self, exercise_id=None, difficulty=None, **kwargs):
-        self._validate_init_args(exercise_id, difficulty)
+    def __init__(self, exercise_id=None, difficulty=None, python_version=3, **kwargs):
+        self._validate_init_args(exercise_id, difficulty, python_version)
         self._exercise_id = exercise_id
         self._exercise_difficulty = difficulty
 
-    def _validate_init_args(self, exercise_id, difficulty):
+    def _validate_init_args(self, exercise_id, difficulty, python_version):
         if not exercise_id or not difficulty:
             raise ValueError('Both exercise id and difficulty have to be specified')
         try:
@@ -18,6 +18,8 @@ class Wyzwanie:  # TODO singleton?
             raise TypeError('Difficulty has to be one of ("easy", "hard")')
         if difficulty not in ('easy', 'hard'):
             raise TypeError('Difficulty has to be one of ("easy", "hard")')
+        if python not in (2, 3):
+            raise ValueError('Python version has to be one of (2, 3)')
 
     def main_function(self, func):
         self._validate_main_function(func)
