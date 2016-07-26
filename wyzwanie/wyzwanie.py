3915806216ff8c99cfae4d36daa4cc12b18db57c
Glenpl <kubamolinski@gmail.com>
2016-07-26 09:43:46 +0200

    added requirements.txt; added Wyzwanie._get_config_file

--- wyzwanie/wyzwanie.py
+++ wyzwanie/wyzwanie.py
@@ -1,9 +1,13 @@
 from inspect import isfunction
 
+import requests
+
 __all__ = ['Wyzwanie']
 
 
 class Wyzwanie:  # TODO singleton?
+    config_url = None
+
     def __init__(self, exercise_id=None, difficulty=None, python_version=3, **kwargs):
         self._validate_init_args(exercise_id, difficulty, python_version)
         self._exercise_id = exercise_id
@@ -18,7 +22,7 @@ class Wyzwanie:  # TODO singleton?
             raise TypeError('Difficulty has to be one of ("easy", "hard")')
         if difficulty not in ('easy', 'hard'):
             raise TypeError('Difficulty has to be one of ("easy", "hard")')
-        if python not in (2, 3):
+        if python_version not in (2, 3):
             raise ValueError('Python version has to be one of (2, 3)')
 
     def main_function(self, func):
@@ -31,4 +35,12 @@ class Wyzwanie:  # TODO singleton?
             raise TypeError('Wyzwanie.main_function has to be used as a function decorator')
 
     def run(self):
+        self._config = self._get_config_file()
+        args = self._get_cli_arguments()
         pass
+
+    def _get_cli_arguments(self):
+        pass
+
+    def _get_config_file(self):
+        return requests.get(config_url.format(self._exercise_id)).json()
